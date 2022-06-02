b3af6f3c0f00358883f7132b65c22c9eafc698ca
tezosmiami <tezosmiami@gmail.com>
2022-06-01 22:54:42 -0400

    . . .@myknash logo

--- pages/index.js
+++ pages/index.js
@@ -101,7 +101,7 @@ export default function Home({ fotos }) {
         <meta name="twitter:creator" content="@tezosmiami"/>
         <meta name="twitter:description" content="hicetnunc foto objkts. . ." />
         <meta name="twitter:title" content="fotographia.xyz"/>
-        <meta name="twitter:image" content="/fotographia.jpg" />
+        <meta name="twitter:image" content="https://gateway.pinata.cloud/ipfs/QmVUaowvovGk7oj1i8QGzxYS7AVpajPmVEbNDQKXmMVApB"/>
      </Head>
       {/* <p>new mints</p>
   <div className='container'>
